cbbaa3861ed8bee16be31537d9866f01c2f21dda
Crommet <wgrommet05@gmail.com>
2025-09-17 14:16:59 -0500

    Update class8.cpp

--- class8.cpp
+++ class8.cpp
@@ -9,6 +9,7 @@ public:
         x = a;
     }
     friend void max(Test2, Test1);
+    friend void min(Test2, Test1);
 };
 class Test1 {
     int y;
@@ -17,6 +18,7 @@ public:
         y = a;
     }
     friend void max(Test2, Test1);
+    friend void min(Test2, Test1);
 };
 void max(Test2 t2, Test1 t1){
     if (t2.x > t1.y)
@@ -24,11 +26,18 @@ void max(Test2 t2, Test1 t1){
     else
         cout << t1.y << " is maximum" << endl;
 }
+void min(Test2 t2, Test1 t1){
+    if (t2.x < t1.y)
+        cout << t2.x << " is minimum" << endl;
+    else
+        cout << t1.y << " is minimum" << endl;
+}
 int main() {
     Test1 t1;
     Test2 t2;
     t1.set_data(10);
     t2.set_data(20);
     max(t2, t1);
+    min(t2, t1);
     return 0;
 }
